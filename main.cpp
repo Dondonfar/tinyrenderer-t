eda34c301931157469c66a104398c683560e07b5
Dondonfar <gafrohlich@outlook.com>
2026-01-30 17:55:54 -0300

    Drawing four segments with simplest approach - with gaps problem

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+  for (float t=0.; t<1.; t+=.02) {
+      int x = std::round( ax + (bx-ax)*t );
+      int y = std::round( ay + (by-ay)*t );
+      framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64; // generates 64x64 image
     constexpr int height = 64;
@@ -15,8 +23,14 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
     framebuffer.set(ax, ay, white); // don't know what this does, don't ask to me yet, still following his tutorial and learning C++
     framebuffer.set(bx, by, white); // for now, this just set a point in the 64x64 space and it color
+    framebuffer.set(cx, cy, white);
 
     framebuffer.write_tga_file("framebuffer.tga"); // generate image
     return 0;
